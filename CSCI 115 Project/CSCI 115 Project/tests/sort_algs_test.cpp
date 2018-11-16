f0ffc43cd323275b5c127105fe45a64f9a2feec0
Aramayis Orkusyan <coffeemeetscode@gmail.com>
2018-11-16 12:09:19 -0800

    feat: Use std::is_sorted to test sort functions.

--- CSCI 115 Project/CSCI 115 Project/tests/sort_algs_test.cpp
+++ CSCI 115 Project/CSCI 115 Project/tests/sort_algs_test.cpp
@@ -10,38 +10,33 @@
 TEST_CASE( "insertion sort" ) {
 
     SECTION( "sorts empty vector" ) {
-        std::vector<int> vec1;
-        std::vector<int> vec2;
-        insertion_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec;
+        insertion_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty sorted vector" ) {
-        std::vector<int> vec1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        insertion_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        insertion_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty reverse sorted vector" ) {
-        std::vector<int> vec1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        insertion_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        insertion_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty unsorted vector" ) {
-        std::vector<int> vec1 = {5, 1, 4, 2, 3, 9, 6, 8, 7, 10};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        insertion_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {5, 1, 4, 2, 3, 9, 6, 8, 7, 10};
+        insertion_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts vector of strings" ) {
-        std::vector<std::string> vec1 = {"c", "f", "a", "g", "e", "b", "d"};
-        std::vector<std::string> vec2 = {"a", "b", "c", "d", "e", "f", "g"};
-        insertion_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<std::string> vec = {"c", "f", "a", "g", "e", "b", "d"};
+        insertion_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 }
 
@@ -51,38 +46,33 @@ TEST_CASE( "insertion sort" ) {
 TEST_CASE( "selection sort" ) {
 
     SECTION( "sorts empty vector" ) {
-        std::vector<int> vec1;
-        std::vector<int> vec2;
-        selection_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec;
+        selection_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty sorted vector" ) {
-        std::vector<int> vec1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        selection_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        selection_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty reverse sorted vector" ) {
-        std::vector<int> vec1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        selection_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        selection_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty unsorted vector" ) {
-        std::vector<int> vec1 = {5, 1, 4, 2, 3, 9, 6, 8, 7, 10};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        selection_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {5, 1, 4, 2, 3, 9, 6, 8, 7, 10};
+        selection_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts vector of strings" ) {
-        std::vector<std::string> vec1 = {"c", "f", "a", "g", "e", "b", "d"};
-        std::vector<std::string> vec2 = {"a", "b", "c", "d", "e", "f", "g"};
-        selection_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<std::string> vec = {"c", "f", "a", "g", "e", "b", "d"};
+        selection_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 }
 
@@ -92,37 +82,32 @@ TEST_CASE( "selection sort" ) {
 TEST_CASE( "bubble sort" ) {
 
     SECTION( "sorts empty vector" ) {
-        std::vector<int> vec1;
-        std::vector<int> vec2;
-        bubble_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec;
+        bubble_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty sorted vector" ) {
-        std::vector<int> vec1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        bubble_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        insertion_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty reverse sorted vector" ) {
-        std::vector<int> vec1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        bubble_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        bubble_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts non-empty unsorted vector" ) {
-        std::vector<int> vec1 = {5, 1, 4, 2, 3, 9, 6, 8, 7, 10};
-        std::vector<int> vec2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        bubble_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<int> vec = {5, 1, 4, 2, 3, 9, 6, 8, 7, 10};
+        bubble_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 
     SECTION( "sorts vector of strings" ) {
-        std::vector<std::string> vec1 = {"c", "f", "a", "g", "e", "b", "d"};
-        std::vector<std::string> vec2 = {"a", "b", "c", "d", "e", "f", "g"};
-        bubble_sort(vec1.begin(), vec1.end());
-        REQUIRE(std::equal(vec1.begin(), vec1.end(), vec2.begin(), vec2.end()));
+        std::vector<std::string> vec = {"c", "f", "a", "g", "e", "b", "d"};
+        bubble_sort(vec.begin(), vec.end());
+        REQUIRE(std::is_sorted(vec.begin(), vec.end()));
     }
 }
